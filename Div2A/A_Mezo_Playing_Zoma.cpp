501b4e43c59c169680f491d4cc40dc196929d635
Gaurav Singh Kabdwal <gskabdwal@gmail.com>
2025-01-08 19:25:44 +0530

    27th

---
+++ Div2A/A_Mezo_Playing_Zoma.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+    int n, m;
+    string s;
+    cin>>n>>s;
+    
+    cout<<n+1<<"\n";
+    
+    return;
+}
+
+
+signed main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin>>t;
+
+    while(t--) solve();
+
+    return 0;
+}
