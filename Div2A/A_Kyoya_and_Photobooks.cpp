0cbadfbba3b87b8104bb792de2510c74cd9fc05b
Gaurav Singh Kabdwal <gskabdwal@gmail.com>
2025-01-09 22:45:44 +0530

    33rd

---
+++ Div2A/A_Kyoya_and_Photobooks.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+    int n, x, y;
+    string s;
+    cin>>s;
+
+    cout<<(s.size()+1)*25+1<<"\n";
+    
+    return;
+}
+
+
+signed main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    //cin>>t;
+
+    while(t--) solve();
+
+    return 0;
+}
